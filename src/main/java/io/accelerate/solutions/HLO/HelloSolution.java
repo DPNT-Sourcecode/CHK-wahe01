3a08f0f6e3912a882f772ed0a6ad3abfdb750295
sbx_user1051 <sbx_user1051@169.254.61.193>
2025-05-20 12:17:54 +0000

    Tue May 20 12:17:54 UTC 2025

--- src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
+++ src/main/java/io/accelerate/solutions/HLO/HelloSolution.java
@@ -3,10 +3,20 @@ package io.accelerate.solutions.HLO;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
-    private static final String DEFAULT = "World"
+    private static final String defaultTo = "World"
     public String hello(String friendName) {
-        String 
+        String toWhom = isStrValid (friendName) ?  friendName.trim(): defaultTo;
+        return "Hello " +  toWhom + "!";
+    }
 
+    private boolean isStrValid (String in){
+        if (in == null || in.trim().isEmpty()) {
+            return false;
+        }
+        else{
+            return true;
+        }
     }
 }
+
 

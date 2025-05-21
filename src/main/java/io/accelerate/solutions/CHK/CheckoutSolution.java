e5045f09cd593a2a9df616e7dd358c467df88457
sbx_user1051 <sbx_user1051@169.254.0.197>
2025-05-21 13:15:08 +0000

    Wed May 21 13:15:08 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -14,5 +14,3 @@ public class CheckoutSolution {
         return totalPriceCalculator.calculateTotal(skus);
     }
 }
-
-
--- src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
+++ src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
@@ -19,4 +19,3 @@ public class ItemsRepo {
         return itemMap.containsKey(sku);
     }
 }
-

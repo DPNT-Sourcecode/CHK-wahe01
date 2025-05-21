405c3c9f5d3e1e4a45ced15b621081ff1d160617
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-05-21 12:42:28 +0000

    Wed May 21 12:42:28 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/TotalPriceCalculator.java
+++ src/main/java/io/accelerate/solutions/CHK/TotalPriceCalculator.java
@@ -14,8 +14,39 @@ public class TotalPriceCalculator {
             return -1;
         }
 
+        // Count occurrences of each SKU
+        Map<String, Integer> skuCount = new HashMap<>();
+        for (char c : skus.toCharArray()) {
+            String sku = String.valueOf(c);
+            if (!itemsRepo.isSkuValid(sku)) {
+                System.out.println("Unknown sku found!");
+                return -1; // Unknown SKU
+            }
+            skuCount.put(sku, skuCount.getOrDefault(sku, 0) + 1);
+        }
+
+        // Compute total price
+        int total = 0;
+        for (Map.Entry<String, Integer> entry : skuCount.entrySet()) {
+            Item item = itemRepository.getItem(entry.getKey());
+            int quantity = entry.getValue();
+            SpecialOffer offer = item.getSpecialOffer();
+
+            if (offer != null) {
+                int offerQty = offer.getQuantityRequired();
+                int offerPrice = offer.getOfferPrice();
+                total += (quantity / offerQty) * offerPrice;
+                total += (quantity % offerQty) * item.getUnitPrice();
+            } else {
+                total += quantity * item.getUnitPrice();
+            }
+        }
+
+        return total;
+
 
     }
 }
+
 
 

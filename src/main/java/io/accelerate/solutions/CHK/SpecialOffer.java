0ed8454482b92cace6df204a609f33982aa0db5d
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-05-21 12:21:28 +0000

    Wed May 21 12:21:28 UTC 2025

---
+++ src/main/java/io/accelerate/solutions/CHK/SpecialOffer.java
@@ -0,0 +1,14 @@
+package io.accelerate.solutions.CHK;
+
+public class SpecialOffer {
+    private final int quantityRequired;
+    private final int offerPrice;
+
+    public SpecialOffer(int quantityRequired, int offerPrice) {
+        this.quantityRequired = quantityRequired;
+        this.offerPrice = offerPrice;
+    }
+
+    
+
+}

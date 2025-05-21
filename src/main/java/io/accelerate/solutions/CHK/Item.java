d41c7a119ee96f1ac569b40143c3797247c0b1a0
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-05-21 12:15:28 +0000

    Wed May 21 12:15:28 UTC 2025

---
+++ src/main/java/io/accelerate/solutions/CHK/Item.java
@@ -0,0 +1,7 @@
+package io.accelerate.solutions.CHK;
+
+public class Item {
+    private String sku;
+    private int unitPrice;
+    private final SpecialOffer specialOffer;
+}
--- src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
+++ src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
@@ -1,4 +1,9 @@
 package io.accelerate.solutions.CHK;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ItemsRepo {
+    private Map <String, Item> itemMap = new HashMap<>();
+    item
 }
+

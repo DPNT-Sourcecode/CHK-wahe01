3e8a7c4b9d21f76242c62d0c379e1f15ad4f4f90
sbx_user1051 <sbx_user1051@169.254.22.245>
2025-05-21 18:52:07 +0000

    Wed May 21 18:52:07 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
+++ src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
@@ -20,6 +20,36 @@ public class ItemsRepo {
         )));
         itemMap.put("F", new Item("F", 10, List.of(new SelFreeItemOffer("F", 3, 2)
         )));
+        itemMap.put("G", new Item("G", 20, List.of()));
+        itemMap.put("H", new Item("H", 10, List.of(
+                new MultiPriceOffer("H", 10, 80),
+                new MultiPriceOffer("H", 5, 45)
+        )));
+        itemMap.put("I", new Item("I", 35, List.of()));
+        itemMap.put("J", new Item("J", 60, List.of()));
+        itemMap.put("K", new Item("K", 80, List.of(
+                new MultiPriceOffer("K", 2, 150)
+        )));
+        itemMap.put("L", new Item("L", 90, List.of()));
+        itemMap.put("M", new Item("M", 15, List.of()));
+        itemMap.put("N", new Item("N", 40, List.of(
+                new FreeItemOffer("N", 3, "M", 1)
+        )));
+        itemMap.put("O", new Item("O", 10, List.of()));
+        itemMap.put("P", new Item("P", 50, List.of(
+                new MultiPriceOffer("P", 5, 200)
+        )));
+        itemMap.put("Q", new Item("Q", 30, List.of(
+                new MultiPriceOffer("Q", 3, 80)
+        )));
+        itemMap.put("R", new Item("R", 50, List.of(
+                new FreeItemOffer("R", 3, "Q", 1)
+        )));
+        itemMap.put("S", new Item("S", 30, List.of()));
+        itemMap.put("T", new Item("T", 20, List.of()));
+        
+
+
     }
 
     public Item getItem (String sku){
@@ -30,3 +60,4 @@ public class ItemsRepo {
         return itemMap.containsKey(sku);
     }
 }
+

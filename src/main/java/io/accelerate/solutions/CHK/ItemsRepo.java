4c90f2ec71b358c17551130687c5e0b6ab94bafc
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-05-21 12:30:28 +0000

    Wed May 21 12:30:28 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
+++ src/main/java/io/accelerate/solutions/CHK/ItemsRepo.java
@@ -5,7 +5,19 @@ import java.util.Map;
 public class ItemsRepo {
     private Map <String, Item> itemMap = new HashMap<>();
     public ItemsRepo () {
+        itemMap.put("A", new Item("A", 50, new SpecialOffer(3, 130)));
+        itemMap.put("B", new Item("B", 30, new SpecialOffer(2, 45)));
+        itemMap.put("C", new Item("C", 20, null));
+        itemMap.put("D", new Item("D", 15, null));
+    }
 
+    public Item getItem (String sku){
+        return itemMap.get(sku);
+    }
+
+    public boolean isSkuValid (String sku){
+        return itemMap.containsKey(sku);
     }
 }
+
 

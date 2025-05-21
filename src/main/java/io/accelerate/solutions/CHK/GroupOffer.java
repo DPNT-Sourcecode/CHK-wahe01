af5376b44bf11c8b0d745e777942ae21b1412ce1
sbx_user1051 <sbx_user1051@169.254.22.245>
2025-05-21 19:12:13 +0000

    Wed May 21 19:12:13 UTC 2025

---
+++ src/main/java/io/accelerate/solutions/CHK/GroupOffer.java
@@ -0,0 +1,24 @@
+package io.accelerate.solutions.CHK;
+import java.util.*;
+
+public class GroupOffer implements Offer {
+    private final List<String> groupSkus;
+    private final int groupSize;
+    private final int groupPrice;
+
+    public GroupOffer(List<String> groupSkus, int groupSize, int groupPrice) {
+        this.groupSkus = groupSkus;
+        this.groupSize = groupSize;
+        this.groupPrice = groupPrice;
+    }
+
+    @Override
+    public int apply(Map<String, Integer> quantities, ItemsRepo itemsRepo) {
+        // Collect all group items and sort by descending unit price
+        List<String> itemsInGroup = new ArrayList<>();
+        
+
+    }
+
+
+}

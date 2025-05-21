1b16028ac1566a4946f29d56808ffe183f68fc67
sbx_user1051 <sbx_user1051@169.254.5.33>
2025-05-21 16:20:11 +0000

    Wed May 21 16:20:11 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/MultiPriceOffer.java
+++ src/main/java/io/accelerate/solutions/CHK/MultiPriceOffer.java
@@ -9,8 +9,21 @@ public class MultiPriceOffer implements Offer {
     @Override
     public int apply(Map<String, Integer> quantities, ItemRepository itemRepository) {
         if (!quantities.containsKey(sku)) return 0;
+        int count = quantities.get(sku);
+        if (count < quantityRequired) return 0;
+        if (count < quantityRequired) {
+            System.out.println("Invalid: quantityRequired = 0");
+            return 0;
+        }
 
-        int
+        Item item = itemRepository.getItem(sku);
+        int unitPrice = item.getUnitPrice();
+
+
+
+        int numOfOffers = count / quantityRequired;
+
 
     }
 }
+

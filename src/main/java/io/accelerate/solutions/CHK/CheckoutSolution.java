0fe0f1107b7156ef95c2b8715a3f92131759810a
sbx_user1051 <sbx_user1051@169.254.9.45>
2025-05-21 12:12:28 +0000

    Wed May 21 12:12:28 UTC 2025

--- src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
+++ src/main/java/io/accelerate/solutions/CHK/CheckoutSolution.java
@@ -3,9 +3,16 @@ package io.accelerate.solutions.CHK;
 import io.accelerate.runner.SolutionNotImplementedException;
 
 public class CheckoutSolution {
+    private TotalPriceCalculator totalPriceCalculator;
+
+    public (CheckoutSolution() {
+
+    }
+
     public Integer checkout(String skus) {
-        
+
 
     }
 }
+
 
